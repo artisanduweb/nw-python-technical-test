4a5be183e8f958226b425f19feda60e5a2a5addd
lartisanduweb <lartisanduweb@gmail.com>
2024-05-27 13:29:26 +0200

    NWT #14: Add FrenchSite variant

--- app/infrastructure/models/__init__.py
+++ app/infrastructure/models/__init__.py
@@ -1,6 +1,6 @@
 from .site import Site
 from .group import Group
 from .site_group import SiteGroup
-from .country_specific import ItalianSite
+from .country_specific import ItalianSite, FrenchSite
 
-__all__ = ["Site", "Group", "SiteGroup", "ItalianSite"]
+__all__ = ["Site", "Group", "SiteGroup", "ItalianSite", "FrenchSite"]
